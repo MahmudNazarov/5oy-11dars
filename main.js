e220ad67ebf44761fbea3b8da2f9fa1a47441088
MahmudNazarov <sh.nazarov@gmail.com>
2024-12-28 14:56:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+    const bgColor = prompt('Enter background color:');
+    const textColor = prompt('Enter text color:');
+    document.body.style.backgroundColor = bgColor;
+    document.body.style.color = textColor;
